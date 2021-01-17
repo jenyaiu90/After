71b4196ee9c5f9c16996670f34efce5acef9c644
Evgeny <iusko.jenya@yandex.ru>
2021-01-17 19:52:06 +0300

    Added Mob class and temporary textures
    Добавлен класс моба и временные текстуры игрока.

---
+++ Source/After/Gameplay/Entity/Mob/Mob.h
@@ -0,0 +1,18 @@
+    ////////////////////////////////////////
+   //        After by SnegirSoft         //
+  // License: GNU GPL v2.0              //
+ //  File: Mob.h                       //
+////////////////////////////////////////
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "../Entity.h"
+#include "Mob.generated.h"
+
+UCLASS()
+class AFTER_API AMob : public AEntity
+{
+	GENERATED_BODY()
+	
+};
